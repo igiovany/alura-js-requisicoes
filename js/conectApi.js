26aeb703e77a60d70a945f60d66f9ad48c68e546
igiovany <105370259+igiovany@users.noreply.github.com>
2023-02-06 22:32:11 -0300

    displaying elements

---
+++ js/conectApi.js
@@ -0,0 +1,11 @@
+async function listVideos() {
+  const connection = await fetch("http://localhost:3000/videos")
+  const convertedConnection = await connection.json()
+
+  return convertedConnection
+}
+
+export const conectApi = {
+  listVideos
+
+}
